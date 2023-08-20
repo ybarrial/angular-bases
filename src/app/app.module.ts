b4c0ce16f030f713227a87fc904ef3f399f716c5
Your Name <yefferson.barrial@avivel.com.pe>
2023-08-20 12:16:15 -0500

    add imports components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    HeroComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
