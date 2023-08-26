643372dbdc70b057b9f3fefa562d0f6cc8d876bd
Your Name <yefferson.barrial@avivel.com.pe>
2023-08-25 23:14:50 -0500

    add-character

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-character.component.scss']
 })
 export class AddCharacterComponent {
+
+  character: Character = {
+    name : '',
+    power: 0,
+  }
+
+  emitCharacter() {
+    console.log(this.character);
+  }
 
 }
